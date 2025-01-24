406defb1e59cc020fd275136464ae267b39b395a
MarkJamesDunbar <57494763+MarkJamesDunbar@users.noreply.github.com>
2025-01-24 01:30:18 +0000

    minor fix

--- app/drug_app.py
+++ app/drug_app.py
@@ -59,7 +59,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type:"
-article = "This app is a part of a CI-CD pipeline demonstration. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+article = "This app is a part of a CI-CD pipeline demonstration."
 
 
 gr.Interface(
